49d4b40fc67efab0297346e6cc83b87e3bfc625d
Oleksandr Halushchak <galushchak.gs@gmail.com>
2023-01-26 00:41:10 +0200

    Added text

--- src/main/java/org/halushchak/Main.java
+++ src/main/java/org/halushchak/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args)
     {
         Calculator calc = new Calculator();
-        System.out.println(calc.getAverageValue3Numbers(4, 2, 6));
+        System.out.println(calc.getAverageValue3Numbers(4, 2, 6), "first commin");
 
 
     }
